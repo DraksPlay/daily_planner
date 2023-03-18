88f73b92cc02086c1f19c70012484af2596142a5
DraksPlay <draksplay@mail.ru>
2023-03-18 17:27:20 +0300

    Создание авторизации, выхода из аккаунта, обзор всех задач пользователя, кнопка для создания

--- daily_planner/tasks/urls.py
+++ daily_planner/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index, name="index"),
+    path('auth/', include('django.contrib.auth.urls')),
+    path("signup/", SignUp.as_view(), name="signup"),
 ]
--- daily_planner/tasks/views.py
+++ daily_planner/tasks/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from .models import Task
 
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html")
+    tasks = []
+    if request.user:
+        tasks = Task.objects.filter(user_id=request.user.id)
+    return render(request, "tasks/index.html", context={"tasks": tasks})
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+
+class LogOut(CreateView):
+    success_url = reverse_lazy("logout")
+    template_name = "registration/logout.html"
